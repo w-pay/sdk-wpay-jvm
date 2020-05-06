ab7a8189bd01d349fbc6691efd8e9bbefbef0fc5
Kieran Simpson <kieran@redcrew.com.au>
2020-05-06 18:22:08 +1000

    Draft of Payment Confirm screen.

--- app/src/main/java/au/com/woolworths/village/app/PaymentConfirm.kt
+++ app/src/main/java/au/com/woolworths/village/app/PaymentConfirm.kt
@@ -3,10 +3,10 @@ package au.com.woolworths.village.app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PaymentConfirm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.payment_confirm)
     }
 }

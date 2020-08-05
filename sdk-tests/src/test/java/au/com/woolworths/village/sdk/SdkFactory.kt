8254257af23e816948ca0fa90b4b1628166061be
Kieran Simpson <kieran@redcrew.com.au>
2020-08-05 19:57:04 +1000

    Move SDK tests to openapi repo

--- sdk-tests/src/test/java/au/com/woolworths/village/sdk/SdkFactory.kt
+++
@@ -1,7 +0,0 @@
-package au.com.woolworths.village.sdk
-
-interface SdkFactory {
-    fun createCustomerApi(): VillageCustomerApiRepository
-
-    fun createMerchantApi(): VillageMerchantApiRepository
-}
